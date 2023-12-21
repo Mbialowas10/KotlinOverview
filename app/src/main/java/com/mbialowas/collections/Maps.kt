1ff09900b1d0c533c18e70d5fccd69b005d72a63
mbialowas10 <mbialowas@rrc.ca>
2023-12-21 15:51:32 -0600

    Map example

--- app/src/main/java/com/mbialowas/collections/Maps.kt
+++ app/src/main/java/com/mbialowas/collections/Maps.kt
@@ -6,6 +6,43 @@ package com.mbialowas.collections
     like a dictionary or hash
  */
 
+fun main(){
+
+    var iMap = mapOf(
+        1 to "http://www.cnn.com",
+        2 to "http://www.rrc.ca",
+        3 to "http://kotlin.org"
+    )
+
+    for (key in iMap.keys){
+        println(iMap[key])
+    }
+
+    print(iMap[2])
+    println("++++++Testing++++++")
+
+    for (key in iMap){
+        println(key)
+    }
+
+    var mMap = mutableMapOf<Int,String>(
+        1 to "Michael bialowas",
+        2 to "Kotlin",
+        3 to "Happy new year!"
+    )
+
+    mMap.put(5,"Maverick")
+
+    for (item in mMap){
+        print(item.key)
+        println(" ->  ${item.value}")
+    }
+
+
+
+
+}
+
 
 
 
